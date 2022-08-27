ae93d25ab0efa582ae86f9d04e8c072cf6a0c04d
DanieLuan <danielluanlourencol@gmail.com>
2022-08-27 16:10:05 -0300

    first commit

---
+++ fun/outros/Primeiro.hs
@@ -0,0 +1,14 @@
+module Primeiro where
+
+errado :: [Char]
+errado = "not good"
+
+five :: Integer
+five = 5
+
+addFive :: Integer -> Integer
+addFive x = addFive(addFive x)
+
+sempreSix :: Integer -> Integer
+sempreSix x = 6 
+ 
