b5eb2883a11248099196a93d91b6d47835fb4e1d
DanieLuan <danielluanlourencol@gmail.com>
2022-08-28 18:39:46 -0300

    basic types and currying + Primeiro.hs

--- fun/aulas/Primeiro.hs
+++ fun/aulas/Primeiro.hs
@@ -7,8 +7,13 @@ five :: Integer
 five = 5
 
 addFive :: Integer -> Integer
-addFive x = addFive(addFive x)
+addFive x = x + 5
 
 sempreSix :: Integer -> Integer
 sempreSix x = 6 
- 
+
+main :: IO ()
+main = do
+    putStrLn "Enter your name, please"
+    userName <- getLine
+    putStrLn $ "Hello, " ++ userName
